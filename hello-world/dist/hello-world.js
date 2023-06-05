1840f1f7fcca53f6900066bd5a3d47cd0c7ff469
CaCaBlocker <umamlikeyou@gmail.com>
2023-06-05 22:07:23 +0900

    first commit

---
+++ hello-world/dist/hello-world.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.sayGoodbye = exports.sayHello = void 0;
+function sayHello() {
+    console.log("Hi");
+}
+exports.sayHello = sayHello;
+function sayGoodbye() {
+    console.log('goodbye');
+}
+exports.sayGoodbye = sayGoodbye;
---
+++ hello-world/src/hello-world.ts
@@ -0,0 +1,7 @@
+export function sayHello() {
+    console.log("Hi");
+}
+
+export function sayGoodbye() {
+    console.log('goodbye');
+}
